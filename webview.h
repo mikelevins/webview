dc854fe5c9637010cf765a722fc2d50228966ea2
Tikara <tiikara93@gmail.com>
2021-03-10 09:06:14 +1000

    clang-format

--- webview.h
+++ webview.h
@@ -1098,8 +1098,8 @@ private:
   };
 
   HRESULT createEnvironment(const std::wstring &userDataFolder,
-                           const std::wstring &currentExeNameW,
-                           webview2_com_handler *webview2ComHandler) const {
+                            const std::wstring &currentExeNameW,
+                            webview2_com_handler *webview2ComHandler) const {
     return CreateCoreWebView2EnvironmentWithOptions(
         nullptr, (userDataFolder + L"/" + currentExeNameW).c_str(), nullptr,
         webview2ComHandler);
